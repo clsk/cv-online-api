25f8cbb26d7dab97cea462e6d99c54e354d046c2
Jusnen <jusnenvolquez@gmail.com>
2015-06-24 11:49:20 -0400

    da error

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = {
         'connectionLimit': 10,
         'host': 'localhost',
         'user': 'root',
-        'password': '',
+        'password': 'Jadiel',
         'database': 'Resumes'
     },
     'database': 'Resumes',
