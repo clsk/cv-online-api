5652d4be93b9d82e25c7c078f6ed1c3b6d034408
Wendy Reyes <wendyrepez@gmail.com>
2015-06-24 14:28:33 -0400

    divide modals according to the functionality

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = {
         'connectionLimit': 10,
         'host': 'localhost',
         'user': 'root',
-        'password': '',
+        'password': 'wendyes',
         'database': 'Resumes'
     },
     'database': 'Resumes',
