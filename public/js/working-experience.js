1cfef19b7966c850e5902cdd72665920a0585100
Wendy Reyes <wendyrepez@gmail.com>
2015-07-21 14:42:41 -0400

    handle the view of cv and edit professional cv

--- public/js/working-experience.js
+++ public/js/working-experience.js
@@ -1,8 +1,21 @@
 $(function() {
+  if (cv) {
+    console.log(cv);
+    // Fill fields
+    cv.work_experiences.forEach(function(work) {
+    });
+  }
+
   $('.add-row').click(function(e) {
     e.preventDefault();
     var row = $(this).parent().children('.repeater-item:first').clone();
-    row.find('input, textarea').val('');
+    var fieldName = row.attr('data-name');
+    var index = $(this).parent().children('.repeater-item').length;
+    var name = fieldName + '[' + index + ']';
+    row.find('input, textarea').each(function() {
+      var newName = $(this).attr('name').replace(fieldName + '[' + (index-1) + ']', name);
+      $(this).val('').attr('name', newName);
+    })
     row.find('.box-tools').removeClass('hide');
     $(this).before(row);
   });
