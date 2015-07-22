50cd13500afb989589d04c9efd0a3cd959d19953
Michael M. Rosario <mchl0208@gmail.com>
2015-07-22 00:55:24 -0400

    Edit profiles validations
    Front-End Validations to Edit Profile Page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,15 +1,36 @@
 $(function() {
 	$('#btn_update_profile').click(function() {
-	    $.post("Update",
-	    {
-	    	name : $('#name').val(),
-			lastname : $('#Lastname').val(),
-			email : $('#email').val(),
-			telefonos : $('#telefonos').val(),
-			paginas : $('#paginasWeb').val(),
-	    },
-	    function(data, status){
-	    	window.location = "/home";
-	    });
+		var email = $('#email').val();
+		var phone = $("#telefono").val();
+
+		if (IsEmail(email) && IsPhoneNumber(phone)) {
+			$.post("Update",
+		    {
+		    	name : $('#name').val(),
+				lastname : $('#Lastname').val(),
+				email : $('#email').val(),
+				telefonos : $('#telefonos').val(),
+				paginas : $('#paginasWeb').val(),
+		    },
+		    function(data, status){
+		    	window.location = "/home";
+		    });
+
+		}
 	});
+
+	function IsEmail(email) {
+	  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	  return regex.test(email);
+	}
+
+	function IsPhoneNumber(phone) {
+		console.log(phone);
+	  if (phone == "") return true;
+	  var regex = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+	  return regex.test(phone.trim());
+	}
+
+	$("#telefono").mask("(999) 999-9999");
+
 });
