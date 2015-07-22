5537555012c538f3b278b5754a24cf7330367943
Alan Alvarez <aalvarez@aliensoft.net>
2015-07-22 17:48:22 -0400

    fix delete user

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = {
         'connectionLimit': 10,
         'host': 'localhost',
         'user': 'root',
-        'password': 'wendyes',
+        'password': '',
         'database': 'Resumes'
     },
     'database': 'Resumes',
--- routes/users.js
+++ routes/users.js
@@ -102,7 +102,7 @@ router.post('/forgotPassword', function(req, res) {
 router.post('/deleteAccount', auth.isLoggedIn,  function(req, res) {
     // find a user whose email is the same as the forms email
     // we are checking to see if the user trying to login already exists
-    GLOBAL.connection.query("SELECT * FROM Users WHERE email = ?", [req.user.email], function(err, rows) {
+    GLOBAL.sqlConnection.query("SELECT * FROM Users WHERE email = ?", [req.user.email], function(err, rows) {
         if (err)
             return done(err);
         if (!rows.length) {
@@ -111,7 +111,7 @@ router.post('/deleteAccount', auth.isLoggedIn,  function(req, res) {
             console.log("Deleting user: "+req.user.email);
             var updateQuery = "UPDATE Users set active = 0 WHERE email = ?";
             console.log(updateQuery);
-            GLOBAL.connection.query(updateQuery,[req.user.email], function(err,rows){
+            GLOBAL.sqlConnection.query(updateQuery,[req.user.email], function(err,rows){
                 res.redirect('/');
             });
         }
