ba508336851229c2098f4a6bbadf62588bde7233
Michael M. Rosario <mchl0208@gmail.com>
2015-07-22 01:25:41 -0400

    Register validations
    Front-End Validations to Register Page

--- public/js/profile.js
+++ public/js/profile.js
@@ -9,7 +9,7 @@ $(function() {
 		    	name : $('#name').val(),
 				lastname : $('#Lastname').val(),
 				email : $('#email').val(),
-				telefonos : $('#telefonos').val(),
+				telefonos : $('#telefono').val(),
 				paginas : $('#paginasWeb').val(),
 		    },
 		    function(data, status){
@@ -18,18 +18,6 @@ $(function() {
 
 		}
 	});
-
-	function IsEmail(email) {
-	  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	  return regex.test(email);
-	}
-
-	function IsPhoneNumber(phone) {
-		console.log(phone);
-	  if (phone == "") return true;
-	  var regex = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-	  return regex.test(phone.trim());
-	}
 
 	$("#telefono").mask("(999) 999-9999");
 
