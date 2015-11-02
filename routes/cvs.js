2509774016af06c5091e8c525d8bfe8503886b48
Alan Alvarez <aalvarez@aliensoft.net>
2015-11-01 21:32:20 -0400

    Create and edit CV

--- routes/cvs.js
+++ routes/cvs.js
@@ -15,35 +15,105 @@ router.post('/create', function(req, res, next) {
             return;
         }
 
-        session.getUser().then(function(user) {
-            if (user == null) {
-                res.status(500).json({message: 'Could not find user. (But found session)'});
-                return;
+        models.CVs.create({
+                            name: name,
+                            template_id: template_id,
+                            created_by: session.user_fb_id
+        }).then(function(cv) {
+            if (req.body['educations'] != null) {
+                educations = req.body.educations;
+                for (education_data in educations) {
+                    education_data['cv_id'] = cv.id;
+                    models.CVEducations.create(education_data);
+                }
             }
 
-            var name = null;
-            if (req.body['name'] != null) {
-                name = req.body.name;
-            }
-
-            if (req.body['template_id'] != null) {
-                var template_id = req.body['template_id'];
-                if template_ids
-                models.Templates.findAll({where: { id: {$in: req.body.template_ids}}}).then(function(templates) {
-                    models.CVs.create({
-                                        name: name,
-                                        template_id: template_id
-                    }).then(function(cv) {
-                        if (req.body['educations'] != null) {
-                            educations = req.body.educations;
-
+            if (req.body['work_experiences'] != null) {
+                work_experiences = req.body.work_experiences;
+                for (work_experience_data in work_experiences) {
+                    work_experience_data['cv_id'] = cv.id;
+                    bullet_points = work_experience_data['bullet_points'];
+                    delete work_experience['bullet_points'];
+                    models.CVWorkExperiences.create(work_experience_data).then(function(work_experience) {
+                        for (bullet_point in bullet_points) {
+                            bullet_point['cv_work_experience_id'] = work_experience.id
+                            models.CVWorkExperienceBulletPoints.create(bullet_point);
                         }
                     });
-                });
+                }
+            }
+
+            if (req.body['fields'] != null) {
+                fields = req.body.fields;
+                for (field_data in fields) {
+                    field_data['cv_id'] = cv.id;
+                    models.CVFields.create(field_data);
+                }
             }
         });
     });
+});
 
+
+router.post('/:cv_id/edit', function(req, res, next) {
+    var session_id = req.headers['x-session-id'];
+    if (session_id == null || session_id == 0) {
+        res.status(401).json({message: 'No session id header received'});
+        return;
+    }
+    models.Sessions.findById(session_id).then(function(session) {
+        if (session == null) {
+            res.status(401).json({message: "Invalid Session ID"});
+            return;
+        }
+
+        models.CVs.findById(cv_id).then(function(cv) {
+            if (cv == null) {
+                res.status(401).json({message: "Could not find CV with that ID"});
+                return;
+            }
+
+            models.CVEducations.destroy({where: {cv_id: cv.id}});
+            if (req.body['educations'] != null) {
+                educations = req.body.educations;
+                for (education_data in educations) {
+                    education_data['cv_id'] = cv.id;
+                    models.CVEducations.create(education_data);
+                }
+            }
+
+            models.CVEducations.destroy({where: {cv_id: cv.id}});
+            models.CVWorkExperiences.findAll({cv_id: cv.id}).then(function (work_experiences) {
+                for (work_experience in work_experiences) {
+                    models.CVWorkExperienceBulletPoints.destroy({where: {cv_work_experience_id: work_experience.id}});
+                }
+                work_experience.destroy();
+            });
+            if (req.body['work_experiences'] != null) {
+                work_experiences = req.body.work_experiences;
+                for (work_experience_data in work_experiences) {
+                    work_experience_data['cv_id'] = cv.id;
+                    bullet_points = work_experience_data['bullet_points'];
+                    delete work_experience['bullet_points'];
+                    models.CVWorkExperiences.create(work_experience_data).then(function(work_experience) {
+                        for (bullet_point in bullet_points) {
+                            bullet_point['cv_work_experience_id'] = work_experience.id
+                            models.CVWorkExperienceBulletPoints.create(bullet_point);
+                        }
+                    });
+                }
+            }
+
+            models.CVFields.destroy({where: {cv_id: cv.id}});
+            if (req.body['fields'] != null) {
+                fields = req.body.fields;
+                for (field_data in fields) {
+                    field_data['cv_id'] = cv.id;
+                    models.CVFields.create(field_data);
+                }
+            }
+        });
+    });
 });
 
 module.exports = router;
